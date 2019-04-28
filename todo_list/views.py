e0ab0cb2eda04426020dbea8c7d3535665aafc6f
Anton <antonliashok@gmail.com>
2019-04-28 20:51:42 +0300

    add: view end template

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(req):
+    return render(req, 'home.html')
+
+def about(req):
+    name = 'Antikarat'
+    return render(req, 'about.html', {'name': name})

18a99a856724e321a48389c270d7e59695f3d1a5
Anton <antonliashok@gmail.com>
2019-05-09 22:47:34 +0300

    fix: add item to DB

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from .models import List
+from .forms import ListForm
 
 # Create your views here.
 def home(req):
-    list = List.objects.all
-    return render(req, 'home.html', {'all_items': list})
+    if req.method == 'POST':
+        form = ListForm(req.POST or None)
+
+        if form.is_valid():
+            form.save()
+            list = List.objects.all
+            return render(req, 'home.html', {'all_items': list})
+
+    else:
+        list = List.objects.all
+        return render(req, 'home.html', {'all_items': list})
 
 def about(req):
     name = 'Antikarat'

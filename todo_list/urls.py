e5f0c59c227bf4036a9fe24f5867968b8f65cc93
Anton <antonliashok@gmail.com>
2019-05-12 18:29:31 +0300

    add: checkbox switcher

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
+    path('delete/<list_id>', views.delete, name='delete'),
+    path('switch_off/<list_id>', views.switch_off, name='switch_off'),
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
 from .forms import ListForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def home(req):
@@ -10,6 +12,7 @@ def home(req):
         if form.is_valid():
             form.save()
             list = List.objects.all
+            messages.success(req, ('it was added!'))
             return render(req, 'home.html', {'all_items': list})
 
     else:
@@ -19,3 +22,21 @@ def home(req):
 def about(req):
     name = 'Antikarat'
     return render(req, 'about.html', {'name': name})
+
+def delete(req, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(req, ('it was removed!'))
+    return redirect('home')
+
+def switch_off(req, list_id):
+    item = List.objects.get(pk=list_id)
+
+    if item.checked == False:
+        item.checked = True
+    else:
+        item.checked = False
+    #item.checked = True
+    item.save()
+    return redirect('home')
+
